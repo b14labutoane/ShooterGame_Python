2b2a0b1b724f79fc560e98f64612683749f6963b
bia <biancabadescu@gmail.com>
2025-04-02 15:31:42 +0300

    tutorial 1 done

--- shooter_tutorial1.py
+++ shooter_tutorial1.py
@@ -14,12 +14,12 @@ class Soldier(pygame.sprite.Sprite):
     def __init__(self, x, y, scale):
         pygame.sprite.Sprite.__init__(self)
         img = pygame.image.load("img/player/Idle/0.png")
-        self.image = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
+        self.img = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
         self.rect = self.img.get_rect()
         self.rect.center = (x, y)
     
     def draw(self):
-        screen.blit(self.image, self.rect)
+        screen.blit(self.img, self.rect)
 
 
 
